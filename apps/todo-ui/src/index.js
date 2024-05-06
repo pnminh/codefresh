e2b607512ec01f98e9fbf50fc72edc11ea247cbc
Minh Pham <pnminh232@gmail.com>
2024-05-06 12:13:38 -0500

    fix lint

--- apps/todo-ui/src/index.js
+++ apps/todo-ui/src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 import {
-	MantineProvider,
-	ColorSchemeProvider,
-	ColorScheme,
 } from '@mantine/core';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
